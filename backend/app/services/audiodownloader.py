23e461c18ff0d46c4a9cb04e1f13aa6b9d66694b
psypherion <sayan84c@gmail.com>
2025-12-19 00:04:21 +0530

    refactor: encapsulate audio downloading into a class and add data directories to gitignore.

--- backend/app/services/audiodownloader.py
+++ backend/app/services/audiodownloader.py
@@ -1,17 +1,23 @@
 import yt_dlp
 
-def download_audio(url, output_path='./downloads/'):
-    ydl_opts = {
-        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '0',  # Best quality
-        }],
-    }
+class AudioDownloader:
+    def __init__(self, user_id: str):
+        self.USER_ID: str = user_id
+
+    def download_audio(self, url, output_path='./downloads/'):
+        ydl_opts = {
+            'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '0',  # Best quality
+            }],
+        }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-# Usage
-download_audio('https://www.youtube.com/watch?v=H62Jfv1DJlU')
+if __name__ == "__main__":
+    user_id = ""
+    downloader = AudioDownloader(user_id)
+    downloader.download_audio('https://www.youtube.com/watch?v=H62Jfv1DJlU')
